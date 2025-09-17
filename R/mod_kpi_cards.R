f7da1c34736f2e151cd9c705ee8b032cca074f13
Tiago Adria Nunes <tiagoadrianunes@gmail.com>
2025-09-17 13:14:14 -0300

    feat: add KPI cards module for energy dashboard

---
+++ R/mod_kpi_cards.R
@@ -0,0 +1,126 @@
+#' KPI Cards UI Function
+#'
+#' @description A shiny Module for displaying KPI value boxes.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd
+#'
+#' @importFrom shiny NS uiOutput
+#' @importFrom bslib layout_columns
+mod_kpi_cards_ui <- function(id) {
+  ns <- NS(id)
+
+  bslib::layout_columns(
+    col_widths = c(4, 4, 4),
+    shiny::uiOutput(ns("total_consumption_box")),
+    shiny::uiOutput(ns("total_emissions_box")),
+    shiny::uiOutput(ns("avg_daily_usage_box"))
+  )
+}
+
+#' KPI Cards Server Functions
+#'
+#' @param id Module ID
+#' @param data_manager Data manager instance
+#' @param filtered_data Reactive filtered data
+#'
+#' @noRd
+#' @importFrom shiny moduleServer renderUI
+mod_kpi_cards_server <- function(id, data_manager, filtered_data) {
+  moduleServer(id, function(input, output, session) {
+    ns <- session$ns
+
+    output$total_consumption_box <- renderUI({
+      data <- filtered_data()
+      create_consumption_value_box(data, data_manager)
+    })
+
+    output$total_emissions_box <- renderUI({
+      data <- filtered_data()
+      create_emissions_value_box(data, data_manager)
+    })
+
+    output$avg_daily_usage_box <- renderUI({
+      data <- filtered_data()
+      create_usage_value_box(data, data_manager)
+    })
+  })
+}
+
+#' Extended KPI Cards UI Function
+#'
+#' @description Extended version with 6 KPI cards
+#'
+#' @param id Module ID
+#'
+#' @noRd
+#'
+#' @importFrom shiny NS uiOutput
+#' @importFrom bslib layout_columns
+mod_kpi_cards_extended_ui <- function(id) {
+  ns <- NS(id)
+
+  tagList(
+    # First row - primary KPIs
+    bslib::layout_columns(
+      col_widths = c(4, 4, 4),
+      shiny::uiOutput(ns("total_consumption_box")),
+      shiny::uiOutput(ns("total_emissions_box")),
+      shiny::uiOutput(ns("avg_daily_usage_box"))
+    ),
+    # Second row - secondary KPIs
+    bslib::layout_columns(
+      col_widths = c(4, 4, 4),
+      shiny::uiOutput(ns("peak_usage_box")),
+      shiny::uiOutput(ns("efficiency_box")),
+      shiny::uiOutput(ns("facilities_count_box"))
+    )
+  )
+}
+
+#' Extended KPI Cards Server Functions
+#'
+#' @param id Module ID
+#' @param data_manager Data manager instance
+#' @param filtered_data Reactive filtered data
+#'
+#' @noRd
+#' @importFrom shiny moduleServer renderUI tagList
+mod_kpi_cards_extended_server <- function(id, data_manager, filtered_data) {
+  moduleServer(id, function(input, output, session) {
+    ns <- session$ns
+
+    # Primary KPIs
+    output$total_consumption_box <- renderUI({
+      data <- filtered_data()
+      create_consumption_value_box(data, data_manager)
+    })
+
+    output$total_emissions_box <- renderUI({
+      data <- filtered_data()
+      create_emissions_value_box(data, data_manager)
+    })
+
+    output$avg_daily_usage_box <- renderUI({
+      data <- filtered_data()
+      create_usage_value_box(data, data_manager)
+    })
+
+    # Secondary KPIs
+    output$peak_usage_box <- renderUI({
+      data <- filtered_data()
+      create_peak_usage_value_box(data, data_manager)
+    })
+
+    output$efficiency_box <- renderUI({
+      data <- filtered_data()
+      create_efficiency_value_box(data, data_manager)
+    })
+
+    output$facilities_count_box <- renderUI({
+      data <- filtered_data()
+      create_facilities_count_value_box(data)
+    })
+  })
+}
